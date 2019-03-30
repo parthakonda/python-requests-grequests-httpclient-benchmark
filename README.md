62cf7c1792521256f2ee188b9160ac4d7ad4e6ee
parthz <parthsaradhi1992@gmail.com>
2019-03-30 10:10:29 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # python-requests-grequests-httpclient-benchmark
 A bench mark program for different requests modules
+
+# Clone
+```
+cd ~/
+git clone git@github.com:parthakonda/python-requests-grequests-httpclient-benchmark.git
+```
+
+# Setup
+```
+cd ~/
+virtualenv -p python3 venv
+```
+
+# Install requirements
+```
+source ~/venv/bin/activate
+pip3 install -r ~/python-requests-grequests-httpclient-benchmark/requirements.txt
+```
+
+# Run
+```
+cd ~/python-requests-grequests-httpclient-benchmark/
+python3 -m unittest benchmark.py
+```
---
+++ benchmark.py
@@ -0,0 +1,96 @@
+import http.client
+import requests
+import unittest
+import time
+import grequests
+import urllib.request
+import urllib.parse
+import gevent.monkey
+gevent.monkey.patch_all()
+
+
+class BenchMarkTest(unittest.TestCase):
+
+    host = 'http://sample.com'
+    path = '/'
+    bench_mark = {
+        'http_client': [],
+        'requests': [],
+        'grequests': [],
+        'urllib': []
+    }
+    NO_OF_TIMES = 20
+    NO_OF_REQUESTS = 20
+
+    def test_a_http_client(self):
+        for _ in range(self.NO_OF_TIMES):
+            start = time.time()
+            for _ in range(self.NO_OF_REQUESTS):
+                if self.host.startswith("http://"):
+                    connection = http.client.HTTPConnection(self.host[7:])
+                else:
+                    connection = http.client.HTTPSConnection(self.host[8:])
+                connection.request('GET', self.path)
+                response = connection.getresponse()
+                connection.close()
+            end = time.time()
+            total = end - start
+            self.bench_mark['http_client'].append(total)
+            # print("Total Time Elapsed for http.client", (end - start))
+
+    def test_b_requests(self):
+        for _ in range(self.NO_OF_TIMES):
+            start = time.time()
+            for _ in range(self.NO_OF_REQUESTS):
+                connection = requests.get('{}{}'.format(self.host, self.path))
+                response = connection.text
+            end = time.time()
+            total = end - start
+            self.bench_mark['requests'].append(total)
+            # print("Total Time Elapsed for requests", (end - start))
+
+    def test_c_grequests(self):
+        for _ in range(self.NO_OF_TIMES):
+            start = time.time()
+            for _ in range(self.NO_OF_REQUESTS):
+                connection = grequests.get('{}{}'.format(self.host, self.path))
+                response = grequests.map([connection])[0].text
+            end = time.time()
+            total = end - start
+            self.bench_mark['grequests'].append(total)
+            # print("Total Time Elapsed for grequests", (end - start))
+
+    def test_d_urllib(self):
+        for _ in range(self.NO_OF_TIMES):
+            start = time.time()
+            for _ in range(self.NO_OF_REQUESTS):
+                f = urllib.request.urlopen('{}{}'.format(self.host, self.path))
+                response = f.read().decode('utf-8')
+            end = time.time()
+            total = end - start
+            self.bench_mark['urllib'].append(total)
+            # print("Total Time Elapsed for urllib", (end - start))
+
+    def test_e_response(self):
+        for i in range(0, self.NO_OF_TIMES + 1):
+            message = "{:*^12}\t".format("") if i == 0 else "{}".format(i)
+            print(message, end='\t')
+
+        print("\n")
+        for i in range(0, self.NO_OF_TIMES + 1):
+            print('----', end='----')
+
+        print("\n")
+        for key, values in self.bench_mark.items():
+            print("{:*^12}   ".format(key), end='\t')
+            for val in values:
+                print("{0:.3f}".format(val), end='\t')
+            print("\n")
+
+        print("\n")
+        print("\n")
+        print("Response:")
+        print("\n")
+        for key, values in self.bench_mark.items():
+            print("{:*^12}   ".format(key), end='\t')
+            print(sum(values)/len(values), end='\n')
